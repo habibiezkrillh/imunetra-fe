6cb31d2b7c1a8e1b2043a2978be7f2663d85a861
leowdij08 <158173182+leowdij08@users.noreply.github.com>
2025-05-13 00:58:37 +0800

    regiter dan dashboard

--- frontendrelawanimunetra/lib/main.dart
+++ frontendrelawanimunetra/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-
+import 'register.dart';
+import 'dashboard.dart'; 
 void main() {
   runApp(const MyApp());
 }
@@ -12,8 +13,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Imunetra',
-      home: const LoginPage(),
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/dashboard': (context) => const DashboardPage(), // Tambahkan ini
+      },
     );
   }
 }
